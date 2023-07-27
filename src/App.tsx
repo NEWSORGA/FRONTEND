a1c43ef1671b9571c81c152260a5d241b3440419
Jonika <66825034+Jonika3000@users.noreply.github.com>
2023-07-27 22:17:07 +0300

    start main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import DefaultLayout from './components/default/Default-Layout';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
+import MainPage from './components/main-page/Main';
 
 function App() {
   return (
     <>
       <Routes>
+        <Route index element={<MainPage></MainPage>} />
         <Route path="/" element={<DefaultLayout />}>
           <Route path='register' element={<Register></Register>} />
           <Route path='login' element={<Login></Login>} />
--- src/components/main-page/Main.tsx
+++ src/components/main-page/Main.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import FadeUp from '../common/animate/Animate'
+import './Main.css'
+
 const MainPage = () => {
     return (
         <>
-        
+            <div className="mainWrapper">
+                <div className="parallax">
+                    <FadeUp>
+                        <div className='TextInPic'>we think the same as you</div>
+                    </FadeUp>
+                    <div className="BrandDiv">
+                        <Link to='/' className="brand">Quixify</Link> 
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 };
