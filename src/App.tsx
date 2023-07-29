2ae27c39535af37672d497a16e47ef0bbf59c93d
novakvova <novakvova@gmail>
2023-07-29 17:27:40 +0300

    Working google is good

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import DefaultLayout from './components/default/Default-Layout';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import MainPage from './components/main-page/Main';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route index element={<MainPage></MainPage>} />
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path='register' element={<Register></Register>} />
-          <Route path='login' element={<Login></Login>} />
-        </Route>
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<DefaultLayout/>}>
+                    <Route index element={<MainPage></MainPage>}/>
+                    <Route path='register' element={<Register></Register>}/>
+                    <Route path='login' element={<Login></Login>}/>
+                </Route>
+            </Routes>
+        </>
+    )
 }
 
 export default App
--- src/components/default/Default-Layout.tsx
+++ src/components/default/Default-Layout.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom"; 
+import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import './Default-Layout.css';
+import {GoogleOAuthProvider} from "@react-oauth/google";
 const DefaultLayout = () => {
     return (
         <div className="DefaultLayout">
             <Header />
-            <Outlet />
+            <GoogleOAuthProvider clientId="763864234936-s953ounakf8bemdaqva8hlao2dai2dj5.apps.googleusercontent.com">
+                <Outlet />
+            </GoogleOAuthProvider>
         </div>
     );
 }
