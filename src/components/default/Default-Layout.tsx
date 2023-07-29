0c7924490042c0eb766191b7fa2cfba7d0206112
Jonika <66825034+Jonika3000@users.noreply.github.com>
2023-07-29 18:19:22 +0300

    little change

--- src/components/default/Default-Layout.tsx
+++ src/components/default/Default-Layout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Header from "../header/Header";
 import './Default-Layout.css';
 import {GoogleOAuthProvider} from "@react-oauth/google";
 const DefaultLayout = () => {
     return (
-        <div className="DefaultLayout">
-            <Header />
+        <div className="DefaultLayout"> 
             <GoogleOAuthProvider clientId="763864234936-s953ounakf8bemdaqva8hlao2dai2dj5.apps.googleusercontent.com">
                 <Outlet />
             </GoogleOAuthProvider>
