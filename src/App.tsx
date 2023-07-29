7f704621d70a7c66ce075c61eb39d01b730e8e11
Jonika <66825034+Jonika3000@users.noreply.github.com>
2023-07-29 18:13:01 +0300

    fix animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './App.css'
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DefaultLayout from './components/default/Default-Layout';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import MainPage from './components/main-page/Main';
-import AnimationPage from "./components/animation/AnimationPage.tsx";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<DefaultLayout/>}>
-                    <Route index element={<MainPage></MainPage>}/>
-                    <Route path='register' element={<Register></Register>}/>
-                    <Route path='login' element={<Login></Login>}/>
+                <Route index element={<MainPage></MainPage>} /> 
+                <Route path="/" element={<DefaultLayout />}>
+                    <Route path='/register' element={<Register></Register>} />
+                    <Route path='/login' element={<Login></Login>} />
                 </Route>
-                <Route path='/animation' element={<AnimationPage/>}/>
             </Routes>
         </>
     )
--- src/components/common/animate/Animate.tsx
+++ src/components/common/animate/Animate.tsx
@@ -1,4 +1,27 @@
-import { FC, PropsWithChildren, CSSProperties, useRef, RefObject, useState, useEffect } from "react";
+
+import {CSSProperties, FC, PropsWithChildren, RefObject,
+    useEffect, useState,useRef} from 'react';
+
+function useElementOnScreen(ref: RefObject<Element>, rootMargin = "0px") {
+    const [isIntersecting, setIsIntersecting] = useState(true);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsIntersecting(entry.isIntersecting);
+            },
+            { rootMargin }
+        );
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, []);
+    return isIntersecting;
+}
 
 const AnimateIn: FC<
     PropsWithChildren<{ from: CSSProperties; to: CSSProperties }>
@@ -27,26 +50,12 @@ const AnimateIn: FC<
         </div>
     );
 };
-function useElementOnScreen(ref: RefObject<Element>, rootMargin = "0px") {
-    const [isIntersecting, setIsIntersecting] = useState(true);
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                setIsIntersecting(entry.isIntersecting);
-            },
-            { rootMargin }
-        );
-        if (ref.current) {
-            observer.observe(ref.current);
-        }
-        return () => {
-            if (ref.current) {
-                observer.unobserve(ref.current);
-            }
-        };
-    }, []);
-    return isIntersecting;
-} 
+
+const FadeIn: FC<PropsWithChildren> = ({ children }) => (
+    <AnimateIn from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {children}
+    </AnimateIn>
+);
 
 const FadeUp: FC<PropsWithChildren> = ({ children }) => (
     <AnimateIn
@@ -56,5 +65,15 @@ const FadeUp: FC<PropsWithChildren> = ({ children }) => (
         {children}
     </AnimateIn>
 );
- 
-export default FadeUp; 
+
+const ScaleIn: FC<PropsWithChildren> = ({ children }) => (
+    <AnimateIn from={{ scale: "0" }} to={{ scale: "1" }}>
+        {children}
+    </AnimateIn>
+);
+
+export const Animate = {
+    FadeIn,
+    FadeUp,
+    ScaleIn
+};
