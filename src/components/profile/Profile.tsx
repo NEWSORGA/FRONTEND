71ec41c6fab0b0f3c2746d17a4fa43ed0daf9fbd
BeVll <vladixerplay@gmail.com>
2023-08-02 21:38:08 +0300

    redesign & like animation

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -47,7 +47,7 @@ const Profile = () => {
             await sleep(1000);
             setLoadPosts(false);
             setPosts(res.data);
-            
+
         });
     }, [])
     return (
@@ -55,57 +55,48 @@ const Profile = () => {
         <>
             <div className="ProfileWrapper d-flex justify-content-center"
                 style={{ backgroundImage: userPage?.backgroundImage != null ? `url(${APP_ENV.BASE_URL + "/images/" + userPage?.backgroundImage})` : "url(https://www.everwallpaper.co.uk/cdn/shop/collections/3D_Wallpaper.jpg?v=1660209305)" }}>
-                <div className="ProfileBackground " style={{ backgroundColor: "rgba(25,25,25,.9)", display: loadProfile ? "flex" : "block" }}>
+                <div className="ProfileBackground " style={{ display: loadProfile ? "flex" : "block" }}>
                     {loadProfile ? <MutatingDots
                         height="100"
                         width="100"
-                        color="#9B2222"
-                        secondaryColor='#9B2222'
+                        color="#EB4C42"
+                        secondaryColor='#EB4C42'
                         radius='12.5'
                         ariaLabel="mutating-dots-loading"
                         wrapperStyle={{}}
                         wrapperClass="loadProfile"
                         visible={loadProfile}
                     /> : (<>
-                        <Row style={{ height: "200px" }}>
-                            <Col xs="3" className='d-flex justify-content-center'>
-                                <div className="ProfileAvatar"
-                                    style={{ backgroundImage: `url(${APP_ENV.BASE_URL + "/images/" + userPage?.image})` }}>
-                                </div>
-                            </Col>
-                            <Col xs="9">
-                                <div className="UserDataProfile m-3 mt-4">
+
+                        <div className='profileLine'>
+                            <div className="ProfileAvatar"
+                                style={{ backgroundImage: `url(${APP_ENV.BASE_URL + "/images/" + userPage?.image})` }}>
+                            </div>
+                            <div>
+                                <div className="UserDataProfile">
                                     <div className="NickNameProfile">
                                         <a>{userPage?.name}</a>
                                     </div>
                                     <div className="CountryProfile">
-                                        <span className={`fi fi-${userPage?.countryCode}`}></span>
+                                        <span className={`fi fi-${userPage?.countryCode} flag`}></span>
                                         <a> {userPage?.country}</a>
                                     </div>
-                                    <div className="StatusProfile mt-3 w-50">
+                                    <div className="StatusProfile">
                                         <a>{userPage?.description}</a>
                                     </div>
                                 </div>
-                            </Col>
-                        </Row>
-                        <div className="WritePost m-3 mt-5">
-                            <Form.Group>
-                                <Form.Label>Whatever you think</Form.Label>
-                                <Form.Control
-                                    placeholder='What?'
-                                    as="textarea"
-                                    className='profileTextArea' />
-                            </Form.Group>
-                            <div className="d-flex justify-content-left">
-                                <div className="btn btnProfile">Send</div>
                             </div>
+                        </div>
+                        <div className="WritePost">
+                            <input className='inputPost' placeholder="What's new?" />
+                            <div className="btn btnProfile">Send</div>
                         </div>
                         <div className="AllPostsProfile" style={{ display: loadPosts ? "flex" : "block" }}>
                             {loadPosts ? <MutatingDots
                                 height="100"
                                 width="100"
-                                color="#9B2222"
-                                secondaryColor='#9B2222'
+                                color="#EB4C42"
+                                secondaryColor='#EB4C42'
                                 radius='12.5'
                                 ariaLabel="mutating-dots-loading"
                                 wrapperStyle={{}}
