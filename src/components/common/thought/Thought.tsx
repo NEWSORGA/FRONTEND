3fb79205ba2920ae7e90dcb033ea3d18c7d6516c
BeVll <vladixerplay@gmail.com>
2023-08-03 15:09:52 +0300

    createPost, redesign photo and header

--- src/components/common/thought/Thought.tsx
+++ src/components/common/thought/Thought.tsx
@@ -10,7 +10,6 @@ const Thought = ({ tweet }: { tweet: ITweetView }) => {
     const [likesCount, setLikesCount] = useState<number>();
     const [gridColumns, setGridColumns] = useState<string>();
     const [gridRows, setGridRows] = useState<string>();
-    const [count, setCount] = useState<number>();
     useEffect(() => {
         setLike(tweet.liked);
         setLikesCount(tweet.likesCount);
@@ -32,13 +31,15 @@ const Thought = ({ tweet }: { tweet: ITweetView }) => {
                 setGridColumns("1fr 1fr");
                 setGridRows("1fr");
                 break;
+            case 4:
+                setGridColumns("1fr 1fr");
+                setGridRows("1fr 1fr");
+                break;
             default:
                 break;
         }
     }
 
-
-    
 
     const likeTweet = () => {
         if (liked == false && likesCount != undefined) {
@@ -87,28 +88,18 @@ const Thought = ({ tweet }: { tweet: ITweetView }) => {
                     {tweet?.tweetText}
                 </div>
                 <div className='images' style={{ gridTemplateColumns: gridColumns, gridTemplateRows: gridRows }}>
-                  
-                    {tweet.medias.map((img) => (
-                        <>
-                            <div className="col position-relative">
-                                <div className="imgUp">
-                                    <div className="imagePreview align-items-center">
-                                        <img
-                                            src={`${APP_ENV.BASE_URL}/images/300_` + img.path}
-                                            className="img-fluid"
-                                            alt="Зображення"
-                                            style={{ width: '100%',  overflow: 'hidden' }}
-                                        />
 
-                                    </div>
-
-
-
-
-                                </div>
+                    {tweet.medias.map((img, i) => (
+                        <div key={img.id} className="col position-relative" style={i == 0 && tweet.medias.length == 3 ? { gridRowStart: 1, gridRowEnd: 3 } : {}}>
+                            <div className="imgUp">
+                                <img
+                                    src={`${APP_ENV.BASE_URL}/images/600_` + img.path}
+                                    className="img-fluid"
+                                    alt="Зображення"
+                                    style={{ height: '100%', width: '100%', overflow: 'hidden' }}
+                                />
                             </div>
-
-                        </>
+                        </div>
                     ))}
                 </div>
                 <div className='actions'>
@@ -135,8 +126,8 @@ const Thought = ({ tweet }: { tweet: ITweetView }) => {
                                 <defs>
 
                                 </defs>
-                                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" sketch: type="MSPage">
-                                    <g id="Icon-Set" sketch: type="MSLayerGroup" transform="translate(-100.000000, -255.000000)" fill="#3E444F">
+                                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" >
+                                    <g id="Icon-Set" transform="translate(-100.000000, -255.000000)" fill="#3E444F">
                                         <path d="M116,281 C114.832,281 113.704,280.864 112.62,280.633 L107.912,283.463 L107.975,278.824 C104.366,276.654 102,273.066 102,269 C102,262.373 108.268,257 116,257 C123.732,257 130,262.373 130,269 C130,275.628 123.732,281 116,281 L116,281 Z M116,255 C107.164,255 100,261.269 100,269 C100,273.419 102.345,277.354 106,279.919 L106,287 L113.009,282.747 C113.979,282.907 114.977,283 116,283 C124.836,283 132,276.732 132,269 C132,261.269 124.836,255 116,255 L116,255 Z" id="comment-1" sketch: type="MSShapeGroup">
 
                                         </path>
